51e193defdc88ad0b249413eb7a54857475117da
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-21 14:03:08 +0200

    file deleted

--- architecturequiz/src/Contexts/Modern.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const Modern = createContext(null)
--- architecturequiz/src/Contexts/Traditional.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const Traditional = createContext(null)
