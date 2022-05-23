6d8550436fd88b1262f28c47e1b59afb582db60f
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-23 15:11:58 +0200

    Result added

--- src/Result.js
+++ src/Result.js
@@ -26,5 +26,319 @@ export default function Result() {
     </div>
   );
 
-  return <div>hh</div>;
+  const Norman = () => (
+    <div className="">
+      <h2>Norman - you like horror stories </h2>{" "}
+      <img src="https://images.pexels.com/photos/4697786/pexels-photo-4697786.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You love to tickle your mind with true crime stories and horror movies,
+        but that does not mean that you are a bad seed yourself. You are just
+        fascinated by the dark and intimidating sides of life and humans. You
+        love to indulge in the psychology of the mind and often practice what
+        you have learned there in the real life.
+      </p>{" "}
+    </div>
+  );
+
+  const Pagode = () => (
+    <div className="">
+      <h2>Pagode - modern believer </h2>{" "}
+      <img src="https://images.pexels.com/photos/3204950/pexels-photo-3204950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are the pagoda as you are a modern believer. You go with the time,
+        but feel a strong connection to your ancestral line and spirituality at
+        the same time. You would love to see more meaningful architecture in
+        your daily life and it makes you a little sad that more and more of that
+        is replaced by soulless buildings. You don’t want to lose touch with
+        what’s really important in life.
+      </p>{" "}
+    </div>
+  );
+
+  const Pyramid = () => (
+    <div className="">
+      <h2>Pyramid - the mystic</h2>{" "}
+      <img src="https://images.pexels.com/photos/12046436/pexels-photo-12046436.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are a pyramid. You are mysterious, but magnetic. Many people look up
+        to you. Not all of them understand you and your impact and yet they are
+        mesmerized by your presence. But this effect didn’t come to you easily.
+        It took many years of work for you to get there, to build up that image
+        and foundation. You got a lot of help and now is the time to give back.
+        You may be or end up as a philanthropist that is involved in many
+        projects that help others.
+      </p>{" "}
+    </div>
+  );
+
+  const Baroque = () => (
+    <div className="">
+      <h2>Baroque - You have class</h2>{" "}
+      <img src="https://images.pexels.com/photos/3229580/pexels-photo-3229580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are Baroque, because your level of class and style is unbeautiful.
+        You are what Anna Denvy always wanted to be without even faking any of
+        it. You probably own a collection of expensive sunglasses or shoes, your
+        hair always smells good and your exclusive accessoires are the cherry on
+        the top. You probably speak French or plan to learn it in the future.
+        You are high class, but you are not kitschy. So you ditch the extra
+        layer of gold to brag with and cut down to what is actually important.
+        You are likely invested into trading. Many look up to you and not a few
+        of them envy you. Through your elegant aura you manage to gain a lot of
+        power and followers and your actions have impact. Being a mentor may be
+        your role.
+      </p>{" "}
+    </div>
+  );
+
+  const ClassicalArchitecture = () => (
+    <div className="">
+      <h2>Classical Architecture - You are content</h2>{" "}
+      <img src="https://images.pexels.com/photos/7907896/pexels-photo-7907896.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You love history and suck it up like a sponge. You are also interested
+        in philosophy and literature. And classical architecture you are meant
+        to just be yourself throughout time. A lot is changing in the world
+        quickly, but your role is to preserve the old, sacred knowledge. People
+        love and respect you as you are and you have the rare privilege to not
+        have to change yourself to fit in.
+      </p>{" "}
+    </div>
+  );
+
+  const Suburban = () => (
+    <div className="">
+      <h2>Suburban Home - Family person</h2>{" "}
+      <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You have a solid picture in your mind of how your life should look and
+        you follow the path that leads you there consequently. Whatever is on
+        your to-do list you get done, but everyone who works hard also should
+        play hard. You enjoy your free time with family and friends and love to
+        invite your neighbors for a barbecue. You are well adjusted to our
+        society and the foundation of it being able to function and run.
+      </p>{" "}
+    </div>
+  );
+
+  const OldNew = () => (
+    <div className="">
+      <h2>Mixing old with new - the story teller</h2>{" "}
+      <img src="https://images.pexels.com/photos/600622/pexels-photo-600622.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are an odd, but inspiring mix of the traditional and modern times.
+        Creativity is your biggest asset and you are a great storyteller. Any
+        party-host is happy to have you there as a guest as you tend to stir
+        things up and to bring life and inspiration to any group you join. You
+        don’t like to throw parties yourself though. Actually you enjoy your
+        solitude and daydreaming. You may be a true introvert. Considering you
+        have a really social way with people and a rather eccentric clothing
+        style, people may be irritated as they perceive you as an extrovert with
+        an endless amount of energy.
+      </p>{" "}
+    </div>
+  );
+
+  const Amsterdam = () => (
+    <div className="">
+      <h2>Amsterdam Style - The cozy one</h2>{" "}
+      <img src="https://images.pexels.com/photos/280189/pexels-photo-280189.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are the cozy type. You don’t like to leave your house or
+        neighborhood too much. You don’t need to see the world. Your environment
+        is what you love and you chose that on purpose. You love your daily
+        routine and you work with what you have. The goals you set aren’t set
+        too high and you rarely exhaust yourself and are basically never
+        stressed out. People love that about you and they feel very safe being
+        around you.
+      </p>{" "}
+    </div>
+  );
+
+  const Bhutanese = () => (
+    <div className="">
+      <h2>Bhutanese - The reflected one</h2>{" "}
+      <img src="https://images.pexels.com/photos/910368/pexels-photo-910368.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are irritated by how fast things go out there in the big cities and
+        decide that it’s not for you anymore. Instead you have spent time
+        building up a retreat for yourself where you can live life at your own
+        pace, reflect and philosophize. This introversion brought you a lot of
+        wisdom that others can perceive. Whenever they need advice they turn to
+        you.
+      </p>{" "}
+    </div>
+  );
+
+  const FirstTier = () => (
+    <div className="">
+      <h2>Chinese First Tier - The hustler</h2>{" "}
+      <img src="https://images.pexels.com/photos/618079/pexels-photo-618079.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        While others seem to deal with burnout and existential crises, you are
+        hustling and winning. You love modern life and feel very comfortable on
+        the fast lane. It is possible that you are a manager or entrepreneur.
+        You don’t mind looking back to the past, but mainly because you use the
+        knowledge for your current projects. It is possible that you may change
+        the world one day with your impact and energy to strive forwards.
+      </p>{" "}
+    </div>
+  );
+
+  const Bauhaus = () => (
+    <div className="">
+      <h2>Bauhaus - Modern pragmatic</h2>{" "}
+      <img src="https://images.unsplash.com/photo-1608700272578-057e251574b3?ixlib=rb-1.2.1&raw_url=true&q=60&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmF1aGF1c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700"></img>{" "}
+      <p>
+        You are a very pragmatic person that loves design. Your appearance is
+        not cluttered with many useless things, but rather things you use daily
+        and that look stylish at the same time. You are a good and respectful
+        listener and give great advice as you focus on the important things in
+        life. Decluttering the world and helping others as their muse is a role
+        that fits you well.
+      </p>{" "}
+    </div>
+  );
+
+  const ArtNouveau = () => (
+    <div className="">
+      <h2>Art Nouveau - The art lover</h2>{" "}
+      <img src="https://c0.wallpaperflare.com/preview/669/36/857/latvia-riga-neustadt-art-nouveau.jpg"></img>{" "}
+      <p>
+        You are in love with art and seek to surround yourself with special,
+        extraordinary things. Going on crazy adventures that expand your mind
+        and creating unique experiences is on the top of your priorities in
+        life. People see you as a muse and you never fail to share your spark
+        and sweet ideas. To come up with those you do need a lot of inspiration
+        yourself as well and you don’t mind picking it up through weekly gallery
+        and museum visits.
+      </p>{" "}
+    </div>
+  );
+
+  const Mosque = () => (
+    <div className="">
+      <h2>Mosque - The spiritual</h2>{" "}
+      <img src="https://images.pexels.com/photos/1537086/pexels-photo-1537086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are very spiritual and on a constant quest to do the right thing.
+        Listening to and helping others is a big part of your life. Life can be
+        tough, but you feel support through your spirituality and that gives you
+        energy, even with rocky parts of the road ahead. Keep going your way.
+        For many you are the oasis of trust and hope they are yearning to find.
+      </p>{" "}
+    </div>
+  );
+
+  const Industrial = () => (
+    <div className="">
+      <h2>Industrial Architecture - The subcultural</h2>{" "}
+      <img src="https://images.pexels.com/photos/3054252/pexels-photo-3054252.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        On the outside you don’t really seem that exciting, but when one gets to
+        know you, it suddenly becomes clear that there is so much to you. You
+        don’t like to spend much time on decorating the fassade and putting on
+        fancy outfits, in fact you see that rather as a waste of time and look
+        down on giving the appearance too much attention. What counts for you is
+        on the inside and you hold much exclusive knowledge there. Therefore it
+        is not surprising that you rather connect with a specific subculture
+        than the mainstream.
+      </p>{" "}
+    </div>
+  );
+
+  const Brutalist = () => (
+    <div className="">
+      <h2>Brutalist - The curious</h2>{" "}
+      <img src="https://images.pexels.com/photos/1835675/pexels-photo-1835675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are very clear and direct and you do not tolerate any disrespect.
+        You strive for power and top positions to make sure you are secured. Is
+        it possible though that sometimes you are provocative and that way start
+        arguments with others? Your motives are not always very clear to others.
+        Something you definitely deserve credit for though, is that you are very
+        curious. When you feel safe, you like to leave your comfort zone and
+        explore new perspectives.
+      </p>{" "}
+    </div>
+  );
+
+  const Contemporary = () => (
+    <div className="">
+      <h2>Contemporary - Living in the moment</h2>{" "}
+      <img src="https://images.pexels.com/photos/3685203/pexels-photo-3685203.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        Your motto is to go with the flow and live in the moment. You are well
+        adjusted to life demands and are very independent, yet dependable.
+        Naturally you don’t have a big ego that stands in your way constantly
+        and you are able to move smoothly through life. Many love how pleasant
+        and approachable you are and love to cooperate with you.
+      </p>{" "}
+    </div>
+  );
+
+  const Glass = () => (
+    <div className="">
+      <h2>Glass buildung - The sensitive one</h2>{" "}
+      <img src="https://images.pexels.com/photos/3735677/pexels-photo-3735677.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are a very sensitive person that values transparency and is a
+        reflection of their environment. You like to open up and share your
+        feelings but that makes you vulnerable. Sometimes people hurt you.
+        Nevertheless you like this trait of yours and wouldn’t stop being you no
+        matter what. That makes you a great source of support and inspiration
+        for others, even if it is hard sometimes. Your contribution to society
+        is immense though and you are appreciated by many.
+      </p>{" "}
+    </div>
+  );
+
+  const reset = () => {
+    setQuestion(0);
+    setModern(0);
+    setTraditional(0);
+    setMainstream(0);
+    setSpecific(0);
+  };
+
+  const Result = () => (
+    <div className="Result">
+      <div className="">
+        {" "}
+        <button onClick={() => reset()} className="">
+          Play again
+        </button>{" "}
+      </div>
+      <h2> You are: </h2>
+      <div className="">
+        {traditional === 0 && mainstream > specific ? <Contemporary /> : null}
+        {traditional === 0 && mainstream < specific ? <Glass /> : null}
+        {traditional === 2 && mainstream > specific ? <FirstTier /> : null}
+        {traditional === 2 && mainstream < specific ? <Bauhaus /> : null}
+        {traditional === 3 && mainstream > specific ? <ArtNouveau /> : null}
+        {traditional === 3 && mainstream < specific ? <Industrial /> : null}
+        {traditional === 4 && mainstream > specific ? <Suburban /> : null}
+        {traditional === 4 && mainstream < specific ? <OldNew /> : null}
+        {traditional === 5 && mainstream > specific ? <Amsterdam /> : null}
+        {traditional === 5 && mainstream < specific ? <Brutalist /> : null}
+        {traditional === 6 && mainstream > specific ? <TimberFraming /> : null}
+        {traditional === 6 && mainstream < specific ? <Pagode /> : null}
+        {traditional === 7 && mainstream > specific ? <Baroque /> : null}
+        {traditional === 7 && mainstream < specific ? <Mosque /> : null}
+        {traditional === 8 && mainstream > specific ? <ClassicalArchitecture /> : null}
+        {traditional === 8 && mainstream < specific ? <Bhutanese /> : null}
+        {traditional === 9 && mainstream > specific ? <Norman /> : null}
+        {traditional === 9 && mainstream < specific ? <Pyramid /> : null}
+      </div>
+      <div className="creator">
+        Cereated by <a href="https://github.com/Reggie899">Reggie899</a>
+      </div>
+    </div>
+  );
+  return (
+    <div>
+      {" "}
+      <Result />
+    </div>
+  );
 }
