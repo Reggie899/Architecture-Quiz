3496afc73ac2323ee531e26c9b15cf8f0477fb80
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-23 12:32:23 +0200

    Result in progress

--- src/Questions/Question9.js
+++ src/Questions/Question9.js
@@ -1,21 +1,64 @@
-import {useContext} from "react";
-import {Mainstream} from "../Contexts/Mainstream"; 
-import {Specific} from "../Contexts/Specific";
-import {Traditional} from "../Contexts/Traditional";
-import {Modern} from "../Contexts/Modern";
-import {QuestionCounter} from "../Contexts/QuestionCounter"
-
-
+import { useContext, useEffect } from "react";
+import { Mainstream } from "../Contexts/Mainstream";
+import { Specific } from "../Contexts/Specific";
+import { Traditional } from "../Contexts/Traditional";
+import { Modern } from "../Contexts/Modern";
+import { QuestionCounter } from "../Contexts/QuestionCounter";
 
 export default function Question9() {
+  const { question, setQuestion } = useContext(QuestionCounter);
+  const { modern, setModern } = useContext(Modern);
+  const { traditional, setTraditional } = useContext(Traditional);
+  const { mainstream, setMainstream } = useContext(Mainstream);
+  const { specific, setSpecific } = useContext(Specific);
 
-return (
+  const traditionalSpecific = () => {
+    setTraditional((traditional) => traditional + 1);
+    setSpecific((specific) => specific + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
 
+  const traditionalMainstream = () => {
+    setTraditional((traditional) => traditional + 1);
+    setMainstream((mainstream) => mainstream + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  const modernSpecific = () => {
+    setModern((modern) => modern + 1);
+    setSpecific((specific) => specific + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  const modernMainstream = () => {
+    setModern((modern) => modern + 1);
+    setMainstream((mainstream) => mainstream + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+      console.log("test -modern:", modern, "mainstream:", mainstream );
+      
+  }, [])
+
+  return (
     <div>
-        
-    </div>
-)
-
-
-
+      <h1>
+        9. You are out with a friend, what drink do you order? 
+      </h1>
+      <div className="">
+    <div onClick={modernSpecific} className=""><p>Just water! Thanks!</p></div>
+    <div onClick={modernSpecific} className=""><p>A tasteful red wine.</p></div>
+    <div onClick={modernSpecific} className=""><p>Whisky for me, please</p></div>
+    <div onClick={modernMainstream} className=""><p>Beer!</p></div>
+    <div onClick={modernMainstream} className=""><p>I’ll check on the happy hour cocktails!</p></div>
+    <div onClick={traditionalSpecific} className=""><p>I’m picking a lemonade or some juice.</p></div>
+    <div onClick={traditionalSpecific} className=""><p>I’ll if they call a drink the speciality of their place and take that. </p></div>
+    <div onClick={modernSpecific} className=""><p>I am a gin lover.</p></div>
+    </div>    </div>
+  );
 }
--- src/Result.js
+++ src/Result.js
@@ -1,21 +1,30 @@
-import {useContext} from "react";
-import {Mainstream} from "./Contexts/Mainstream"; 
-import {Specific} from "./Contexts/Specific";
-import {Traditional} from "./Contexts/Traditional";
-import {Modern} from "./Contexts/Modern";
-import {QuestionCounter} from "./Contexts/QuestionCounter"
-
-
+import { useContext } from "react";
+import { Mainstream } from "./Contexts/Mainstream";
+import { Specific } from "./Contexts/Specific";
+import { Traditional } from "./Contexts/Traditional";
+import { Modern } from "./Contexts/Modern";
+import { QuestionCounter } from "./Contexts/QuestionCounter";
 
 export default function Result() {
+  const { question, setQuestion } = useContext(QuestionCounter);
+  const { modern, setModern } = useContext(Modern);
+  const { traditional, setTraditional } = useContext(Traditional);
+  const { mainstream, setMainstream } = useContext(Mainstream);
+  const { specific, setSpecific } = useContext(Specific);
 
-return (
-
-    <div>
-        
+  const TimberFraming = () => (
+    <div className="">
+      <h2>The traditionalist</h2>{" "}
+      <img src="https://images.pexels.com/photos/2886284/pexels-photo-2886284.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></img>{" "}
+      <p>
+        You are timber framing as you love traditions, old objects and
+        relationships that have proven stable. You are not living in the past
+        though. Basically you carefully filter what is worth preserving and then
+        take care of those chosen things carefully. The result is that you
+        surround yourself with beautiful things that deserve your attention.
+      </p>{" "}
     </div>
-)
+  );
 
-
-
+  return <div>hh</div>;
 }
