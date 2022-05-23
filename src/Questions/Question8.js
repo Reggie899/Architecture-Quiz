06cf600766820b5bfd13fd9df09b2867d7905095
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-23 12:12:11 +0200

    Question8 options added

--- src/Questions/Question8.js
+++ src/Questions/Question8.js
@@ -1,21 +1,60 @@
-import {useContext} from "react";
-import {Mainstream} from "../Contexts/Mainstream"; 
-import {Specific} from "../Contexts/Specific";
-import {Traditional} from "../Contexts/Traditional";
-import {Modern} from "../Contexts/Modern";
-import {QuestionCounter} from "../Contexts/QuestionCounter"
-
-
+import { useContext, useEffect } from "react";
+import { Mainstream } from "../Contexts/Mainstream";
+import { Specific } from "../Contexts/Specific";
+import { Traditional } from "../Contexts/Traditional";
+import { Modern } from "../Contexts/Modern";
+import { QuestionCounter } from "../Contexts/QuestionCounter";
 
 export default function Question8() {
+  const { question, setQuestion } = useContext(QuestionCounter);
+  const { modern, setModern } = useContext(Modern);
+  const { traditional, setTraditional } = useContext(Traditional);
+  const { mainstream, setMainstream } = useContext(Mainstream);
+  const { specific, setSpecific } = useContext(Specific);
 
-return (
+  const traditionalSpecific = () => {
+    setTraditional((traditional) => traditional + 1);
+    setSpecific((specific) => specific + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
 
+  const traditionalMainstream = () => {
+    setTraditional((traditional) => traditional + 1);
+    setMainstream((mainstream) => mainstream + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  const modernSpecific = () => {
+    setModern((modern) => modern + 1);
+    setSpecific((specific) => specific + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  const modernMainstream = () => {
+    setModern((modern) => modern + 1);
+    setMainstream((mainstream) => mainstream + 1);
+    setQuestion((question) => question + 1);
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+      console.log("test -modern:", modern, "mainstream:", mainstream );
+      
+  }, [])
+
+  return (
     <div>
-        
-    </div>
-)
-
-
-
+      <h1>
+        8. Would you consider plastic surgery? 
+      </h1>
+      <div className="">
+    <div onClick={traditionalMainstream} className=""><p>No way!</p></div>
+    <div onClick={modernMainstream} className=""><p>No, but I might change my mind. Who knows.</p></div>
+    <div onClick={modernMainstream} className=""><p>I am thinking about it.</p></div>
+    <div onClick={traditionalSpecific} className=""><p>Yes, absolutely!</p></div>
+    </div>    </div>
+  );
 }
